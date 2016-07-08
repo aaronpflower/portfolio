f53bdb9847832f81183bf8c504c5d0adc6ed4246
Aaron Flower <aaronpflower@gmail.com>
2016-07-08 14:21:32 -0600

    fixing index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 var bodyParser = require('body-parser')
+var port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.static('dist'))
@@ -12,9 +13,11 @@ app.use(bodyParser.json({limit : "50mb"}))
 require('./server/routes/sendEmail.js')(app)
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('index');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+	var host = this.address().address;
+	var x = this.address().port;
+  console.log('Example app listening on port', host, x);
 });
