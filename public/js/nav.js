21766b77e2bb74bbee5b07c466a40e888ed58508
Aaron Flower <aaronpflower@gmail.com>
2016-07-22 12:04:50 -0700

    nav animation wip

--- public/js/nav.js
+++ public/js/nav.js
@@ -9,23 +9,23 @@ Nav.controller = {
 Nav.view = {
 	init: function() {
 		var firstSection = document.getElementById('top').offsetHeight;
-		var scrollTime = firstSection / 4;
+		var scrollTime = firstSection / 6;
 		console.log(scrollTime)
 		window.addEventListener('scroll', function(){
-			var nav = document.getElementById('nav');
+			var navItemWrapper = document.getElementById('nav-item-wrapper');
 			var aboutLi = document.getElementById('about-li');
 			var workLi = document.getElementById('work-li');
 			var contactLi = document.getElementById('contact-li');
 
-			if(window.pageYOffset >= scrollTime) {
-				nav.classList.add('nav-scroll')
+			if(window.pageYOffset > 0) {
+				navItemWrapper.classList.add('nav-scroll')
 				aboutLi.classList.add('about-li-animate')
 				workLi.classList.add('work-li-animate')
 				contactLi.classList.add('contact-li-animate')
 			}
 
 			else if(window.pageYOffset <= scrollTime) {
-				nav.classList.remove('nav-scroll')
+				navItemWrapper.classList.remove('nav-scroll')
 				aboutLi.classList.remove('about-li-animate')
 				workLi.classList.remove('work-li-animate')
 				contactLi.classList.remove('contact-li-animate')
